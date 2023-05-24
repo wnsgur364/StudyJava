f44e48594b444b8302db22501bbaad7cf62037cb
wnsgur364 <wnsgur364@naver.com>
2023-05-24 17:28:59 +0900

    Exc0524

---
+++ src/com/mycompany/study/ch02/Exc0524.java
@@ -0,0 +1,17 @@
+package com.mycompany.study.ch02;
+
+public class Exc0524 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("HelloWorld");
+		
+		int a = 1;
+		boolean b = true;
+		boolean c = false;
+		
+		System.out.println(a + ", " + b + ", " + c);
+	}
+
+}
--- src/com/mycompany/study/ch02/HelloJava.java
+++ src/com/mycompany/study/ch02/HelloJava.java
@@ -2,4 +2,40 @@ package com.mycompany.study.ch02;
 
 public class HelloJava {
 	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		  선언	=	초기화		
+		boolean a = true;
+		boolean b = false;
+
+		int c = 1;
+		long d = 10000000000000L;
+		
+		double e = 3;
+		
+		char f = 1;
+		
+		System.out.println("c:" + c);
+		System.out.println("d:" + d);
+		System.out.println("a:" + a);
+		System.out.println("b:" + b);
+		System.out.println("e:" + e);
+		System.out.println("f:" + f);
+		
+//		변수 선언: 데이터타입 변수며이
+//		변수 초기화: 데이터타입 변수명 = 리터럴;
+		
+		int r = 132;
+		int t;
+		t = 133;
+		int y,u,i;
+		y = 134;
+		u = 135;
+		i = 136;
+		
+		System.out.println(r + "," + t + "," + y + "," + u + "," + i);
+		
+		
+	}
+
 }
