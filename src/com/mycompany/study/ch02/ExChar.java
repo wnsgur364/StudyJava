3e87a210d4471b6abe781efcfddb6ca02f6d68f5
wnsgur364 <wnsgur364@naver.com>
2023-05-25 16:52:09 +0900

    f.ch02.first

--- src/com/mycompany/study/ch02/ExChar.java
+++ src/com/mycompany/study/ch02/ExChar.java
@@ -3,7 +3,6 @@ package com.mycompany.study.ch02;
 public class ExChar {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
 		char c1 = 'A';			// 문자
 		char c2 = 65;			// 10진수
@@ -12,6 +11,21 @@ public class ExChar {
 		System.out.println ("c1: " + c1);
 		System.out.println ("c2: " + c2);
 		System.out.println ("c3: " + c3);
+		
+		char c4 = '가';			// 문자
+		char c5 = 44032;			// 10진수
+		char c6 = '\uac00';		// 16진수
+		
+		System.out.println ("----------");
+		System.out.println ("c4: " + c4);
+		System.out.println ("c5: " + c5);
+		System.out.println ("c6: " + c6);
+		
+		int uniCode = c1;
+		System.out.println ("uniCode: " + uniCode);
+		
+		int uniCode2 = c4;
+		System.out.println ("uniCode2: " + uniCode2);
 	}
 
 }
--- src/com/mycompany/study/ch02/ExFloatDouble.java
+++ src/com/mycompany/study/ch02/ExFloatDouble.java
@@ -11,12 +11,18 @@ public class ExFloatDouble {
 		float var4 = 3.14f;
 		float var5 = 3.14F;
 		
+		double var6 = 1234567;		// ~7자리
+		double var7 = 12345678;		// 8자리~ 1.2345678E7 > E = 10, 7 = 10의 -7제곱
+		
 		System.out.println("var1: " + var1);
 		System.out.println("var2: " + var2);
 		
 		System.out.println("var3: " + var3);
 		System.out.println("var4: " + var4);
 		System.out.println("var5: " + var5);
+		
+		System.out.println("var6: " + var6);
+		System.out.println("var7: " + var7);
 		
 	}
 
