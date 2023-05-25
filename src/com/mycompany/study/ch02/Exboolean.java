bdcfebb963696e56597dc2edbc701a6b7e5aa6c5
wnsgur364 <wnsgur364@naver.com>
2023-05-25 15:37:06 +0900

    boolean

---
+++ src/com/mycompany/study/ch02/Exboolean.java
@@ -0,0 +1,30 @@
+package com.mycompany.study.ch02;
+
+public class Exboolean {
+
+	public static void main(String[] args) {
+		
+		// 선언
+		// 변수명은 defaultNy 로 불리언 변수 선언
+		boolean defaultNy;
+		
+		
+		// 초기화
+		// 변수명은 default1Ny로 불리언 변수 선언 초기값은 true
+		boolean default1Ny = true;
+		
+		// 변수명은 default2Ny로 불리언 변수 선언 초기값은 false
+		boolean default2Ny = false;
+		
+		// default1Ny값이 참이면 "기본값입니다." 아니면 "여분값입니다." 출력 하시요
+		
+		// 괄호안이 참이면 if 구문 아니면 else 구문
+		if (default1Ny) {
+			System.out.println ("기본값입니다.");
+		} else {
+			System.out.println ("여분값입니다.");
+		}
+		
+	}
+
+}
---
+++ src/com/mycompany/study/ch02/Trboolean.java
@@ -0,0 +1,18 @@
+package com.mycompany.study.ch02;
+
+public class Trboolean {
+
+	public static void main(String[] args) {
+		
+		boolean defaultNy1 = true;
+		boolean defaultNy2 = false;
+		
+		if (defaultNy1) {
+			System.out.println ("기본값입니다.");
+		} else {
+			System.out.println ("여분값입니다.");
+		}
+
+	}
+
+}
