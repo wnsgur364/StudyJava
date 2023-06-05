c42cbe9f058aadab770936f5728a98f4ce612fc3
wnsgur364 <wnsgur364@naver.com>
2023-06-05 15:57:44 +0900

    2

---
+++ src/com/mycompany/study/ch04/ExRandom.java
@@ -0,0 +1,16 @@
+package com.mycompany.study.ch04;
+
+public class ExRandom {
+
+    public static void main(String[] args) {
+        
+        System.out.println(Math.random());
+
+
+
+
+
+        
+    }
+
+}
