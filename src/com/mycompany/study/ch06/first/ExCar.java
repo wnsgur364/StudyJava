5c99071ef56d5b926b345642a12f5c7d6791b9b7
wnsgur364 <wnsgur364@naver.com>
2023-06-08 16:25:12 +0900

    끝?

--- src/com/mycompany/study/ch06/first/ExCar.java
+++ src/com/mycompany/study/ch06/first/ExCar.java
@@ -6,7 +6,7 @@ public class ExCar {
         int a = 1;
         System.out.println("a: " + a);
         
-        Car car = new Car();    // default 생성자
+        Car car = new Car();    // 기본생성자
 
         System.out.println("차종: " + car.name);
         System.out.println("색상: " + car.color);
